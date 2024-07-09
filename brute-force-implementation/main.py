6a0239d2be54f6fc0774ea85c4a3f0ba1fc11b86
Felix Merz <felixmerz00@gmail.com>
2024-07-09 14:15:55 +0200

    cleanup

--- brute-force-implementation/main.py
+++ brute-force-implementation/main.py
@@ -21,9 +21,7 @@ k_b = -1
 # algorithm 1 Alizade Nikoo
 
 
-# PAA
-# TODO: Normalization missing
-# TODO: Check if this PAA corresponds to the 
+# PAA custom
 print('log info: paa 1')
 k = 100   # reduced window size
 data_reduced = np.arange(len(data)*(k/n))
@@ -34,12 +32,9 @@ for i in range(len(data)//n):  # iterate through windows, i is the i-th window
     seg_len = n//k
     start_idx_seg_k = start_idx_win_i + j * seg_len
     idx_reduced = int(i*k+j)
-    # print(f"idx_reduced: {idx_reduced}, start_idx_seg_k:start_idx_seg_k+seg_len={start_idx_seg_k}:{start_idx_seg_k+seg_len}")
     data_reduced[idx_reduced] = np.mean(data[start_idx_seg_k:start_idx_seg_k+seg_len]) # calculate mean of segment
-    # break
-  # break
 
-# PAA version 2 with pyts package
+# PAA with pyts package
 print('log info: paa 2')
 from pyts.approximation import PiecewiseAggregateApproximation
 # window_size in this case is what I call segment size
@@ -47,8 +42,9 @@ paa = PiecewiseAggregateApproximation(window_size=n//k)
 data_win = data[:500]
 data_win_reshaped = data_win.reshape(1, -1)
 data_win_reduced = paa.transform(data_win_reshaped)
-print(np.shape(data_win_reduced))
 
+# the first 100 data points in reduced representation (the first 20 elements of reduced data) 
+# are identical between my custom solution and the PAA from pyts, but the data has a different
+# shape
 print("First 100 data points reduced representation:", data_reduced[0:20], "\n")
-print("First 100 data points reduced representation with pyts:", data_win_reduced, "\n")
-print("Last 5 data points reduced representation:", data_reduced[-5:], "\n")
+print("First 500 data points reduced representation with pyts:", data_win_reduced, "\n")
