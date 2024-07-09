2eb8ea8c215a8e35162fda5921234b800e3a4086
Felix Merz <felixmerz00@gmail.com>
2024-07-09 14:59:13 +0200

    move paa to separate file

--- brute-force-implementation/main.py
+++ brute-force-implementation/main.py
@@ -1,7 +1,5 @@
 import librosa  # librosa is not compatible with numpy 2
-import numpy as np
-# print(np.__version__)   # check if I am using a version of numpy < 2
-import logging
+from paa import paa_custom, paa_pyts
 
 # load the audio file (relative path to from where you run main.py)
 print('log info: loading audio file')
@@ -20,31 +18,13 @@ k_b = -1
 
 # algorithm 1 Alizade Nikoo
 
-
-# PAA custom
-print('log info: paa 1')
+# PAA
 k = 100   # reduced window size
-data_reduced = np.arange(len(data)*(k/n))
-i = 0
-for i in range(len(data)//n):  # iterate through windows, i is the i-th window
-  start_idx_win_i = i*500
-  for j in range(k):  # iterate through k equi-length segments
-    seg_len = n//k
-    start_idx_seg_k = start_idx_win_i + j * seg_len
-    idx_reduced = int(i*k+j)
-    data_reduced[idx_reduced] = np.mean(data[start_idx_seg_k:start_idx_seg_k+seg_len]) # calculate mean of segment
-
-# PAA with pyts package
-print('log info: paa 2')
-from pyts.approximation import PiecewiseAggregateApproximation
-# window_size in this case is what I call segment size
-paa = PiecewiseAggregateApproximation(window_size=n//k)
-data_win = data[:500]
-data_win_reshaped = data_win.reshape(1, -1)
-data_win_reduced = paa.transform(data_win_reshaped)
+data_reduced = paa_custom(data, n, k)
+data_win_reduced = paa_pyts(data, n, k)
 
 # the first 100 data points in reduced representation (the first 20 elements of reduced data) 
 # are identical between my custom solution and the PAA from pyts, but the data has a different
 # shape
-print("First 100 data points reduced representation:", data_reduced[0:20], "\n")
-print("First 500 data points reduced representation with pyts:", data_win_reduced, "\n")
+# print("First 100 data points reduced representation:", data_reduced[0:20], "\n")
+# print("First 500 data points reduced representation with pyts:", data_win_reduced, "\n")
---
+++ brute-force-implementation/paa.py
@@ -0,0 +1,31 @@
+import numpy as np
+# librosa is not compatible with numpy 2
+# check if I am using a version of numpy < 2
+# print(np.__version__)
+from pyts.approximation import PiecewiseAggregateApproximation
+
+# PAA custom
+def paa_custom(data, n: int, k: int):
+  print('log info: paa 1')
+  data_reduced = np.arange(len(data)*(k/n))
+  i = 0
+  for i in range(len(data)//n):  # iterate through windows, i is the i-th window
+    start_idx_win_i = i*500
+    for j in range(k):  # iterate through k equi-length segments
+      seg_len = n//k
+      start_idx_seg_k = start_idx_win_i + j * seg_len
+      idx_reduced = int(i*k+j)
+      data_reduced[idx_reduced] = np.mean(data[start_idx_seg_k:start_idx_seg_k+seg_len]) # calculate mean of segment
+  return data_reduced
+
+# PAA with pyts package
+# !!! does only the first window !!!
+def paa_pyts(data, n: int, k: int):
+  print('log info: paa 2')
+  # window_size in this case is what I call segment size
+  paa = PiecewiseAggregateApproximation(window_size=n//k)
+  data_win = data[:500]
+  data_win_reshaped = data_win.reshape(1, -1)
+  data_win_reduced = paa.transform(data_win_reshaped)
+  return data_win_reduced
+
