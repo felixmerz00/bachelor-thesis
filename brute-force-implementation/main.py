973f92d8d63347fdaadc073146e062568c4a025b
Felix Merz <felixmerz00@gmail.com>
2024-07-09 14:10:06 +0200

    fix custom paa

--- brute-force-implementation/main.py
+++ brute-force-implementation/main.py
@@ -33,7 +33,7 @@ for i in range(len(data)//n):  # iterate through windows, i is the i-th window
   for j in range(k):  # iterate through k equi-length segments
     seg_len = n//k
     start_idx_seg_k = start_idx_win_i + j * seg_len
-    idx_reduced = int(i*(k/n)+j)
+    idx_reduced = int(i*k+j)
     # print(f"idx_reduced: {idx_reduced}, start_idx_seg_k:start_idx_seg_k+seg_len={start_idx_seg_k}:{start_idx_seg_k+seg_len}")
     data_reduced[idx_reduced] = np.mean(data[start_idx_seg_k:start_idx_seg_k+seg_len]) # calculate mean of segment
     # break
@@ -42,13 +42,13 @@ for i in range(len(data)//n):  # iterate through windows, i is the i-th window
 # PAA version 2 with pyts package
 print('log info: paa 2')
 from pyts.approximation import PiecewiseAggregateApproximation
-paa = PiecewiseAggregateApproximation(window_size=n)
-data_reshaped = data.reshape(1, -1)
-data_reduced_2 = paa.transform(data_reshaped)
-print(np.shape(data_reduced_2))
+# window_size in this case is what I call segment size
+paa = PiecewiseAggregateApproximation(window_size=n//k)
+data_win = data[:500]
+data_win_reshaped = data_win.reshape(1, -1)
+data_win_reduced = paa.transform(data_win_reshaped)
+print(np.shape(data_win_reduced))
 
-print("First 100 data points:", data[0:100], "\n")
 print("First 100 data points reduced representation:", data_reduced[0:20], "\n")
-print("First 100 data points reduced representation with pyts:", data_reduced_2[0:20], "\n")
-print("Last 5 data points reduced representation:", data_reduced[-1], "\n")
-print("Last 5 data points reduced representation with pyts:", data_reduced[-1], "\n")
+print("First 100 data points reduced representation with pyts:", data_win_reduced, "\n")
+print("Last 5 data points reduced representation:", data_reduced[-5:], "\n")
