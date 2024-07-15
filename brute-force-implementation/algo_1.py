16df5dc38e19a55e484417a7f759db52b1df4468
Felix Merz <felixmerz00@gmail.com>
2024-07-15 14:53:54 +0200

    change data type of W_e

--- brute-force-implementation/algo_1.py
+++ brute-force-implementation/algo_1.py
@@ -7,14 +7,13 @@ def algorithm_1(t_series, n: int, h: int, T: int, k_s: int, k_e: int, k_b: int):
   print('log info: algorithm 1')
   epsilon_1 = sqrt(2*k_s*(1-T)/n)
   epsilon_2 = sqrt(2*k_e*(1-T)/n)
-  # alpha = 1   # only needed when working with data streams
   m = len(t_series)   # number of time series
 
   # initial windows
   w = [None for _ in range(m)]
   W = [None for _ in range(m)]
   W_s = np.empty((m, k_s))
-  W_e = [None for _ in range(m)]
+  W_e = np.empty((m, k_e))
 
   alpha = 0
   while alpha*h <= (len(t_series[0])-n):  # I assume all time series have the same length
@@ -31,6 +30,6 @@ def algorithm_1(t_series, n: int, h: int, T: int, k_s: int, k_e: int, k_b: int):
       print("SVD computation does not converge.")
     # From the output of SVD we choose the first kb dimensions for the bucketing.
     # From which matrix do they choose the dimensions?
-    break
+    # break
 
     alpha += 1
