e8c99c44f627a2da8aaa0516c51ceec0fb044471
Felix Merz <felixmerz00@gmail.com>
2024-07-21 12:22:59 +0200

    bucketing filter

--- brute-force-implementation/bucketing_filter.py
+++ brute-force-implementation/bucketing_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import floor, ceil
+import itertools
 
 def floor_epsilon(number, eps):
   """
@@ -13,9 +14,40 @@ def ceil_epsilon(number, eps):
   """
   return ceil(number / eps) * eps
 
+def get_neighbors(bkt_cords, k_b: int, B:int):
+  """
+  Generate the coordinates for each neighbor of bucket of bkt.
+  I start from bkt_cords and generating all moves to an adjacent 
+  bucket. The result is the list of moves added to the bkt_cords.
+
+  Parameters:
+  bkt_cords (tuple): Coordinates of your bucket.
+  B (int): The bound of the array (exclusive).
+
+  Returns:
+  list of tuples: A list with the coordinates of all neighbors of bkt.
+  """
+  all_moves = list(itertools.product([-1,0,1], repeat=k_b))
+  all_moves.remove(k_b*(0,))
+  # Adding the moves to the current position gives all virtual neighbors
+  neighbors =  bkt_cords + np.array(all_moves)
+  # ... but we must remove the impossible positions
+  neighbors = neighbors[((neighbors>=0)&(neighbors<B)).all(axis=1)]
+  return [tuple(row) for row in neighbors]
+
 def bucketing_filter(W_b, k_b: int, eps):
+  """
+  Parameters:
+  W_b (numpy.ndarray): Matrix of windows.
+  k_b (int): number of dimensions.
+  eps (float): distance threshold ε
+  
+  Returns:
+  C_1 (set of tuples): Candidate set of indices of likely correlated pairs of windows.
+  """
   m = len(W_b)
-  C_1 = np.empty((pow(m, 2), 2), dtype=int)
+  # C_1 = np.empty((pow(m, 2), 2), dtype=int)
+  C_1 = set()
   
   # initialize k_b-dimensional bucketing scheme
   bkt_lwr_bnd = floor_epsilon(np.min(W_b), eps)
@@ -24,8 +56,8 @@ def bucketing_filter(W_b, k_b: int, eps):
   BKT_dim = k_b*(int(B),)
   BKT = np.full(BKT_dim, fill_value=None, dtype=object)  # each bucket contains a tuple of time series indices, thus dtype=object
 
+  # assign W_b[p] to a bucket
   for p in range(m):
-    # Assign time series to bucket
     bkt = ()
     for w in W_b[p]:
       dim_bkt = floor((w-bkt_lwr_bnd)/eps)
@@ -33,4 +65,23 @@ def bucketing_filter(W_b, k_b: int, eps):
     if BKT[bkt] is None:
       BKT[bkt] = (p,)
     else:
-      BKT[bkt] += (p,)       
+      BKT[bkt] += (p,)
+
+  # check Eucledian distance of W_b[p]s in same and neighboring buckets
+  for index, bkt in np.ndenumerate(BKT):
+    if not bkt is None:
+      # same bucket
+      for i in bkt:
+        for j in bkt:
+          if i < j and np.linalg.norm(W_b[i] - W_b[j]) <= eps:
+            C_1.add((i,j))
+      # neighboring buckets
+      neighbors_idx_lst = get_neighbors(index, k_b, B)
+      for nb_idx in neighbors_idx_lst:
+        if not BKT[nb_idx] is None:
+          for i in bkt:
+            for j in BKT[nb_idx]:
+              if i < j and np.linalg.norm(W_b[i] - W_b[j]) <= eps:
+                C_1.add((i,j))
+  
+  return C_1
